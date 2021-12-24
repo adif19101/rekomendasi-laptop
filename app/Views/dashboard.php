3b4ec49077ab62ab75f07b34c7938c61179c9591
adif19101 <muhammad.adif19101@student.unsika.ac.id>
2021-12-24 12:44:27 +0700

    layout admin otw filter

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -1,8 +1,8 @@
-<?= $this->extend('layout/login_layout') ?>
+<?= $this->extend('layout/admin_layout') ?>
 
 <?= $this->section('content') ?>
 <section class="jumbotron text-center">
-    <h1>Welcome, <?= session()->name ?></h1>
+    <h1>Welcome, <?= session()->nama_ad ?></h1>
     <p>Untuk logout dari sistem silakan klik <a href="<?php echo base_url('logout');?>">Logout</a></p>
 </section>
 
---
+++ app/Views/layout/admin_header.php
@@ -0,0 +1,14 @@
+<header class="jumbotron jumbotron-fluid">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="h1">Selamat Datang, <?= session()->nama_ad ?></h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <h6 class="h6"></h6>
+            </div>
+        </div>
+    </div>
+</header>
